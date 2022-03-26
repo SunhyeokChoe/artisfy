2a70dfdd576f4c34f4e63f54cef6a33123a90fc1
SunhyeokChoe <chltnsgur2@daum.net>
2022-03-26 23:30:41 +0900

    feat(announcement): add announcement component

---
+++ src/components/Announcement.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './Announcement.style'
+
+const Announcement: React.FC = (): JSX.Element => {
+  return (
+    <Container>
+      회원 가입 EVENT. 신규 가입 후 바로 사용 가능한 15% 할인 쿠폰 / Artisfy 스탠다드 990원 구매 기회
+    </Container>
+  )
+}
+
+export default Announcement
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 import Navbar from 'components/Navbar'
+import Announcement from 'components/Announcement'
 
 const Home: React.FC = (): JSX.Element => {
-  return <Navbar />
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+    </div>
+  )
 }
 
 export default Home
