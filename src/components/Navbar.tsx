dc95f963e6c4f527022a3de69927fe207e3df3ba
SunhyeokChoe <chltnsgur2@daum.net>
2022-03-26 22:37:09 +0900

    chore(module): add material-ui/code, material-ui/icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 60px;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 20px;
+`
+
+const Left = styled.div`
+  flex: 1;
+`
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`
+
+const SearchContainer = styled.div``
+
+const Center = styled.div`
+  flex: 1;
+`
+
+const Right = styled.div`
+  flex: 1;
+`
 
 const Navbar: React.FC = (): JSX.Element => {
-  return <div>Navbar</div>
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>input icon</SearchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  )
 }
 
 export default Navbar
