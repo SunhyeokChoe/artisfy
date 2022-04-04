746bc6b0f7a12514f4ea52403a7d6922a9b0dc3a
SunhyeokChoe <chltnsgur2@daum.net>
2022-04-05 04:05:52 +0900

    style(Categories): fix for responsive

--- src/components/styles/Categories.style.tsx
+++ src/components/styles/Categories.style.tsx
@@ -4,4 +4,8 @@ export const Container = styled.div`
   display: flex;
   padding: 20px;
   justify-content: space-between;
+  ${({ theme }) => theme.media.sm`
+    padding: 0px;
+    flex-direction: column;
+  `}
 `
